77ea5e17904e8e207e27a9e10be80c12745e14d0
Elliot <ellthrowaway2@icloud.com>
2024-05-03 18:22:43 -0400

    songs sorted by title

--- script.js
+++ script.js
@@ -100,6 +100,13 @@ const renderSongs = (array) => {
         .join('')
     playlistSongs.innerHTML = songsHTML
 }
-const sortSongs = () => { }
+const sortSongs = () => {
+    userData?.songs.sort((a, b) => {
+        if (a.title < b.title) { return -1 }
+        if (a.title > b.title) { return 1 }
+        return 0
+    })
+    return userData?.songs
+}
 
-renderSongs(userData?.songs)
+renderSongs(sortSongs())
