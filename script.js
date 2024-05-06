c59f245cd3267535fd5bf3ace38c8cd627dd1454
Elliot <ellthrowaway2@icloud.com>
2024-05-06 17:10:25 -0400

    reset button functioning

--- script.js
+++ script.js
@@ -187,7 +187,10 @@ const setPlayerDisplay = () => {
 
 const setPlayButtonAccessibleText = () => {
     const song = userData?.currentSong || userData?.songs[0]
-    playButton = playButton.setAttribute("aria-label", song?.title ? `"Play ${song.title}` : "Play")
+    playButton = playButton.setAttribute(
+        'aria-label',
+        song?.title ? `"Play ${song.title}` : 'Play'
+    )
 }
 
 const shuffle = () => {
@@ -213,6 +216,11 @@ const deleteSong = (id) => {
     setPlayButtonAccessibleText()
     if (userData?.songs.length === 0) {
         const resetButton = document.createElement('button')
+        const resetText = document.createTextNode('Reset Playlist')
+        resetButton.id = 'reset'
+        resetButton.ariaLabel = 'Reset playlist'
+        resetButton.appendChild(resetText)
+        playlistSongs.appendChild(resetButton)
     }
 }
 
@@ -227,5 +235,11 @@ pauseButton.addEventListener('click', pauseSong)
 nextButton.addEventListener('click', playNextSong)
 previousButton.addEventListener('click', playPreviousSong)
 shuffleButton.addEventListener('click', shuffle)
+resetButton.addEventListener('click', () => {
+    userData.songs = [...allSongs]
+    renderSongs(sortSongs())
+    setPlayButtonAccessibleText()
+    resetButton.remove()
+})
 
 renderSongs(sortSongs())
